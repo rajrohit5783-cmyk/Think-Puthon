d89a54fc222ca62fd17e20173cc28618c2876f33
Rohit Raj <rajrohit5783@gmail.com>
2026-09-01 14:49:05 +0530

    Python ass1 and Ass 2

---
+++ Assignment_01.py
@@ -0,0 +1,89 @@
+
+
+# print Even Numbers
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
+
+#
+
+i = 1
+while i <= 50:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
+    
+    
+#Factorial
+n = int(input("enter the req number:"))
+
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial = factorial * i
+
+print("Factorial =" ,factorial)
+
+
+#
+i = 1
+while i <= n:
+    factorial = factorial * i
+    i=i+1
+
+print("Factorial =", factorial)
+
+
+#no of digits in a number
+
+
+
+
+
+
+
+#Palindrome
+
+n = int(input("enter the number: "))
+
+original = n
+reverse = 0
+
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = int(n/10)
+
+if original == reverse:
+    print("It is a Palindrome number")
+else:
+    print("it is not a Palindrome number")
+    
+#
+
+
+
+#Fibonacci Series
+
+n = int(input("enter the number of terms:"))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a)
+
+    c = a + b
+    a = b
+    b = c
+    
+#
+
+i = 0
+while i < n:
+    print(a)
+    c = a + b
+    a = b
+    b = c
+    i = i + 1
+
---
+++ First.py
@@ -0,0 +1,65 @@
+# # # # # # # Name = "Rohit"
+# # # # # # # print(id(Name))
+# # # # # # # Name = "Rohan"
+# # # # # # # print(id(Name))
+
+# # # # # # # A = 9.6
+# # # # # # # print(id(A))
+# # # # # # # A = 9.6
+# # # # # # # print(id(A))
+
+# # # # # # # B = input('Enter Your Name: ')
+# # # # # # # print(B)
+# # # # # # # print(type(B))
+
+# # # # # # # C = int(input('Enter Your No: '))
+# # # # # # # print(C)
+# # # # # # # print(type(C))
+# # # # # # D = int(float(input('Enter Your Number: ')))
+# # # # # # # D = bool(input('Enter Your Number: '))
+# # # # # # E = D + 10
+# # # # # # print(E)
+# # # # # # print(type(E))
+# # # # # for a in range(5,16):
+# # # # #     print(a)
+
+# # # # a = 15
+# # # # while(a<=30):
+# # # #     print(a)
+# # # #     a += 1
+# # # A = int(input("Enter The Number: "))
+# # # B = input("Enter Your Name: ")
+# # # C = 1
+
+# # # # while(A >= C):
+# # # #     print(B)
+# # # #     A -= 1
+
+
+
+# # A = int(input('Enter The Number: '))
+
+# # # for b in range(1,11):
+# # #     print(A , "X" , b , "=" , A * b)
+# # #     b+=1
+ 
+ 
+# # b = 1
+# # while(b<=10):  
+# #    print(A , "X" , b , "=" , A * b)
+# #    b+=1
+
+S = 0
+i = 1
+# for i in range(1,21):
+#     S= S+i
+#     print(S)
+    
+    
+
+# while(i<=20):
+#   S = S+i
+#   print(S)
+#   i+=1
+
+
